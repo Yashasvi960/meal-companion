a95494775b592e00cb3943bb3a586bac1f231286
Yashasvi <yashasvikumar960@gmail.com>
2026-01-21 18:11:57 -0500

    feat(backend): adding backend to the repo

---
+++ backend/mealCompanionBackend/src/main/java/com/project/mealCompanionBackend/MealCompanionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.mealCompanionBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealCompanionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealCompanionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/mealCompanionBackend/src/test/java/com/project/mealCompanionBackend/MealCompanionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mealCompanionBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealCompanionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
